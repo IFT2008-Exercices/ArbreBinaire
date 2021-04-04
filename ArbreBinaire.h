24018cffa106cb58769459f6c4f31cf886c79065
Alexandre Brown <alexandre.brown@microdrones.com>
2021-04-04 14:13:18 -0400

    Code formatting

--- ArbreBinaire.h
+++ ArbreBinaire.h
@@ -11,229 +11,219 @@
 
 #include <queue>
 
+template<typename TValeur>
+class ArbreBinaire {
 
-template <typename TValeur>
-class ArbreBinaire
-{
-  private:
-    
-#include "noeud.h"
-  
-public:
+	private:
 
-#include "iterator.h"
+		#include "noeud.h"
 
-public:
+	public:
 
-  ArbreBinaire () : racine (nullptr) { }
-  
-  ArbreBinaire(const ArbreBinaire& source) : racine (nullptr) {
-    if (!source.racine) return ; 
-    
-     std::queue<Noeud*> enAttente ; 
-    Noeud* courant = source.racine ; 
-    enAttente.push(courant) ; 
-    
-    while (!enAttente.empty()) {
-        courant = enAttente.front() ; 
-        enAttente.pop () ; 
-        if (courant->reqDroite ()) enAttente.push(courant->reqDroite ()) ; 
-        if (courant->reqGauche ()) enAttente.push(courant->reqGauche ()) ; 
-        inserer( courant->reqValeur () ) ; 
-      } 
-  }
+		#include "iterator.h"
 
-  virtual
-  ~ArbreBinaire ()
-  {
-    Noeud* courant = minimumArbre ();
-    while (courant != nullptr)
-      {
-        Noeud* suivant = successeur (courant);
-        delete courant;
-        courant = suivant;
-      }
-  }
-  
-  const ArbreBinaire& operator = (ArbreBinaire autre) {
-    Noeud* temp = this->racine ; 
-    this->racine = autre.racine ; 
-    autre.racine = temp ; 
-    return (*this) ; 
-  }
-  
-  void visiterParNiveau(void (*callback)(Noeud*)) {
-    if (!racine) return ; 
-    
-    std::queue<Noeud*> enAttente ; 
-    Noeud* courant = racine ; 
-    enAttente.push(courant) ; 
-    
-    while (!enAttente.empty()) {
-        courant = enAttente.front() ; 
-        enAttente.pop () ; 
-        if (courant->reqDroite ()) enAttente.push(courant->reqDroite ()) ; 
-        if (courant->reqGauche ()) enAttente.push(courant->reqGauche ()) ; 
-        (*callback)(courant) ; 
-      } 
-  }
+		ArbreBinaire() : racine(nullptr) {}
 
-  Noeud*
-  minimumDuSousArbre (Noeud* depart) const
-  {
-    Noeud* courant = depart;
-    while (courant->reqGauche ()) courant = courant->reqGauche ();
-    return courant;
-  }
+		ArbreBinaire(const ArbreBinaire& source) : racine(nullptr) {
+			if (source.racine==nullptr) {
+				return;
+			}
 
-  Noeud*
-  minimumArbre () const
-  {
-    if (!racine) return nullptr;
-    return minimumDuSousArbre (racine);
-  }
+			std::queue<Noeud*> enAttente;
+			Noeud* courant = source.racine;
+			enAttente.push(courant);
 
-  Noeud*
-  maximumDuSousArbre (Noeud* depart) const
-  {
-    Noeud* courant = depart;
-    while (courant->reqDroite ()) courant = courant->reqDroite ();
-    return courant;
-  }
+			while (!enAttente.empty()) {
+				courant = enAttente.front();
+				enAttente.pop();
+				if (courant->reqDroite()) {
+					enAttente.push(courant->reqDroite());
+				}
+				if (courant->reqGauche()) {
+					enAttente.push(courant->reqGauche());
+				}
+				inserer(courant->reqValeur());
+			}
+		}
 
-  Noeud*
-  maximumArbre () const
-  {
-    if (!racine) return nullptr;
-    return maximumDuSousArbre (racine);
-  }
+		virtual ~ArbreBinaire() {
+			Noeud* courant = minimumArbre();
+			while (courant!=nullptr) {
+				Noeud* suivant = successeur(courant);
+				delete courant;
+				courant = suivant;
+			}
+		}
 
-  Noeud*
-  successeur (Noeud* courant) const
-  {
-    if (courant->reqDroite () != nullptr) return minimumDuSousArbre (courant->reqDroite ());
-    Noeud* suivant = courant->reqParent ();
-    while ((suivant != nullptr) and (courant == suivant->reqDroite ()))
-      {
-        courant = suivant;
-        suivant = suivant->reqParent ();
-      }
-    return suivant;
-  }
+		const ArbreBinaire& operator=(ArbreBinaire autre) {
+			Noeud* temp = this->racine;
+			this->racine = autre.racine;
+			autre.racine = temp;
+			return *this;
+		}
 
-  const_iterator
-  cdebut () const
-  {
-    return const_iterator (this, minimumArbre ());
-  }
+		void visiterParNiveau(void (* callback)(Noeud*)) {
+			if (racine==nullptr) {
+				return;
+			}
 
-  iterator
-  debut () const
-  {
-    return iterator (minimumArbre ());
-  }
+			std::queue<Noeud*> enAttente;
+			Noeud* courant = racine;
+			enAttente.push(courant);
 
-  const_iterator
-  cfin () const
-  {
-    return const_iterator (this, nullptr);
-  }
+			while (!enAttente.empty()) {
+				courant = enAttente.front();
+				enAttente.pop();
+				if (courant->reqDroite()) {
+					enAttente.push(courant->reqDroite());
+				}
+				if (courant->reqGauche()) {
+					enAttente.push(courant->reqGauche());
+				}
+				(*callback)(courant);
+			}
+		}
 
-  iterator
-  fin () const
-  {
-    return iterator (this, nullptr);
-  }
+		Noeud* minimumDuSousArbre(Noeud* depart) const {
+			Noeud* courant = depart;
+			while (courant->reqGauche()) {
+				courant = courant->reqGauche();
+			}
+			return courant;
+		}
 
-  void
-  visiteEnOrdreDansLeSousArbre (Noeud* sousArbre, std::vector<TValeur>& resultat) const
-  {
-    if (!sousArbre) return;
-    visiteEnOrdreDansLeSousArbre (sousArbre->reqGauche (), resultat);
-    resultat.push_back (sousArbre->reqValeur ());
-    visiteEnOrdreDansLeSousArbre (sousArbre->reqDroite (), resultat);
-  }
+		Noeud* minimumArbre() const {
+			if (racine==nullptr) {
+				return nullptr;
+			}
+			return minimumDuSousArbre(racine);
+		}
 
-  std::vector<TValeur>
-  visiteEnOrdre () const
-  {
-    std::vector<TValeur> v;
-    visiteEnOrdreDansLeSousArbre (racine, v);
-    return v;
-  }
+		Noeud* maximumDuSousArbre(Noeud* depart) const {
+			Noeud* courant = depart;
+			while (courant->reqDroite()) {
+				courant = courant->reqDroite();
+			}
+			return courant;
+		}
 
-  void
-  insererDansLaFeuille (Noeud& noeud, Noeud* feuille)
-  {
-    noeud.asgParent (*feuille);
-    feuille->asgEnfant (noeud);
-  }
+		Noeud* maximumArbre() const {
+			if (racine==nullptr) {
+				return nullptr;
+			}
+			return maximumDuSousArbre(racine);
+		}
 
-  void
-  insererDansLeSousArbre (Noeud& noeud, Noeud* sousArbre)
-  {
-    Noeud* destination = (noeud < *sousArbre) ? sousArbre->reqGauche () : sousArbre->reqDroite ();
-    if (!destination) insererDansLaFeuille (noeud, sousArbre);
-    else insererDansLeSousArbre (noeud, destination);
-  }
+		Noeud* successeur(Noeud* courant) const {
+			if (courant->reqDroite()!=nullptr) {
+				return minimumDuSousArbre(courant->reqDroite());
+			}
+			Noeud* suivant = courant->reqParent();
+			while ((suivant!=nullptr) and (courant==suivant->reqDroite())) {
+				courant = suivant;
+				suivant = suivant->reqParent();
+			}
+			return suivant;
+		}
 
-  void
-  inserer (const TValeur& val)
-  {
-    Noeud* nouveauNoeud = new Noeud (val);
-    if (estVide ()) racine = nouveauNoeud;
-    else insererDansLeSousArbre (*nouveauNoeud, racine);
-  }
-  
-  void supprimer(Noeud* noeud) {
-    return;
-  }
+		const_iterator cdebut() const {
+			return const_iterator(this, minimumArbre());
+		}
 
-  bool
-  leSousArbreEstVide (Noeud* sousArbre) const
-  {
-    return !sousArbre;
-  }
+		iterator debut() const {
+			return iterator(minimumArbre());
+		}
 
-  bool
-  estVide () const
-  {
-    return leSousArbreEstVide (racine);
-  }
+		const_iterator cfin() const {
+			return const_iterator(this, nullptr);
+		}
 
-  const_iterator
-  chercher (const TValeur& valeur) const
-  {
-    return chercherDansLeSousArbre (valeur, racine);
-  }
+		iterator fin() const {
+			return iterator(this, nullptr);
+		}
 
-  const_iterator
-  chercherDansLeSousArbre (const TValeur& valeur, Noeud* debut) const
-  {
-    if (!debut) return cfin ();
-    if (valeur == debut->reqValeur ()) return const_iterator (this, debut);
-    if (valeur < debut->reqValeur ()) return chercherDansLeSousArbre (valeur, debut->reqGauche ());
-    return chercherDansLeSousArbre (valeur, debut->reqDroite ());
-  }
+		void visiteEnOrdreDansLeSousArbre(Noeud* sousArbre, std::vector<TValeur>& resultat) const {
+			if (sousArbre==nullptr) {
+				return;
+			}
+			visiteEnOrdreDansLeSousArbre(sousArbre->reqGauche(), resultat);
+			resultat.push_back(sousArbre->reqValeur());
+			visiteEnOrdreDansLeSousArbre(sousArbre->reqDroite(), resultat);
+		}
 
-  bool
-  lesSousArbresSontSimilaires (const Noeud* lhs, const Noeud* rhs) const
-  {
-    if (!(lhs and rhs)) return (!(lhs or rhs));
-    return lesSousArbresSontSimilaires (lhs->reqGauche (), rhs->reqGauche ()) and lesSousArbresSontSimilaires (lhs->reqDroite (), rhs->reqDroite ());
-  }
+		std::vector<TValeur> visiteEnOrdre() const {
+			std::vector<TValeur> v;
+			visiteEnOrdreDansLeSousArbre(racine, v);
+			return v;
+		}
 
-  bool
-  estSimilaireA (const ArbreBinaire& autre)
-  {
-    return lesSousArbresSontSimilaires (this->racine, autre.racine);
-  }
+		void insererDansLaFeuille(Noeud& noeud, Noeud* feuille) {
+			noeud.asgParent(*feuille);
+			feuille->asgEnfant(noeud);
+		}
 
-private:
-  Noeud* racine;
+		void insererDansLeSousArbre(Noeud& noeud, Noeud* sousArbre) {
+			Noeud* destination = (noeud < *sousArbre) ? sousArbre->reqGauche() : sousArbre->reqDroite();
+			if (!destination) {
+				insererDansLaFeuille(noeud, sousArbre);
+			} else {
+				insererDansLeSousArbre(noeud, destination);
+			}
+		}
+
+		void inserer(const TValeur& val) {
+			auto nouveauNoeud = new Noeud(val);
+			if (estVide()) {
+				racine = nouveauNoeud;
+			} else {
+				insererDansLeSousArbre(*nouveauNoeud, racine);
+			}
+		}
+
+		void supprimer(Noeud* noeud) {
+			// TODO
+		}
+
+		bool leSousArbreEstVide(Noeud* sousArbre) const {
+			return sousArbre==nullptr;
+		}
+
+		bool estVide() const {
+			return leSousArbreEstVide(racine);
+		}
+
+		const_iterator chercher(const TValeur& valeur) const {
+			return chercherDansLeSousArbre(valeur, racine);
+		}
+
+		const_iterator chercherDansLeSousArbre(const TValeur& valeur, Noeud* debut) const {
+			if (debut==nullptr) {
+				return cfin();
+			}
+			if (valeur==debut->reqValeur()) {
+				return const_iterator(this, debut);
+			}
+			if (valeur < debut->reqValeur()) {
+				return chercherDansLeSousArbre(valeur, debut->reqGauche());
+			}
+			return chercherDansLeSousArbre(valeur, debut->reqDroite());
+		}
+
+		bool lesSousArbresSontSimilaires(const Noeud* lhs, const Noeud* rhs) const {
+			if (!(lhs and rhs)) {
+				return (!(lhs or rhs));
+			}
+			return lesSousArbresSontSimilaires(lhs->reqGauche(), rhs->reqGauche())
+				and lesSousArbresSontSimilaires(lhs->reqDroite(), rhs->reqDroite());
+		}
+
+		bool estSimilaireA(const ArbreBinaire& autre) {
+			return lesSousArbresSontSimilaires(this->racine, autre.racine);
+		}
+
+	private:
+		Noeud* racine;
 };
 
-
-
-#endif /* ARBREBINAIRE_H */
+#endif
 
